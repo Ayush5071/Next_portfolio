ccac72171e77cb979efc4c83d28f20ffdd282232
Ayush5071 <ayusht5071@gmail.com>
2025-02-05 03:50:38 +0530

    feat: update metadata in layout; enhance description and add Open Graph support with icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ayush's Portfolio",
-  description: "Excellence in frontend as well as backend development withe knowledge of DSA.",
+  description: "Expert in web development, backend, and DSA.",
+  keywords: ["web development", "backend", "DSA", "Ayush", "portfolio"],
+  icons: {
+    icon: "/ayush.ico",
+    apple: "/ayush.jpg",
+  },
+  openGraph: {
+    title: "Ayush's Portfolio",
+    description: "Expert in web development, backend, and DSA.",
+    images: [
+      {
+        url: "/ayush.jpg",
+        width: 800,
+        height: 600,
+        alt: "Ayush's Portfolio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
